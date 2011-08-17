8fa078c7c41c3695670bab307556862c1d39bdfd
Michael Drake Ranieri <michael.d.ranieri@gmail.com>
2011-08-17 11:44:28 -0400

    Bug fixes

--- isles/core/send_tell.py
+++ isles/core/send_tell.py
@@ -9,20 +9,21 @@ import shelve
 
 def main(params):
     msg, user, channel, users = params
+
+    user = user.encode('utf-8').lower()
     
-    user = user.encode('utf-8')
     db = shelve.open('tShelve')
     try:
         temp, db[user] = db[user], []
         db.close()
     except:
         sys.exit(0)
-    
+
     if temp != []:
         print "Hey %s! You have %s messages." % (user, len(temp))
         for t in temp:
             print '%s said "%s"' % (t['sender'], t['msg'])
-    
+
 
 if __name__ == '__main__':
     params = json.loads(sys.argv[1])
--- isles/core/store_tell.py
+++ isles/core/store_tell.py
@@ -9,31 +9,32 @@ import shelve
 
 def main(params):
     msg, user, channel, users = params
-    
+
     receiver, msg = re.split(':', msg, 1)
-    
+
+    receiver = receiver.lower()
+
     for k, u in users.iteritems():
-        if receiver == k and re.search('H', u['status']):
+        if receiver == k.lower() and re.search('H', u['status']):
             sys.exit(0)
-            
+
     db = shelve.open('tShelve')
     try:
         temp = db[receiver.encode('utf-8')]
     except:
         temp = []
-        
+
     temp.append({
         'sender' : user,
         'msg' : msg
     })
-    
+
     db[receiver.encode('utf-8')] = temp
     db.close()
-    
+
     print "Don't worry %s! I will make sure to give %s that message." % (user, receiver)
-    
+
 
 if __name__ == '__main__':
     params = json.loads(sys.argv[1])
     main(params)
-    
